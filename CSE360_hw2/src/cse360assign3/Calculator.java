26abde149759ee770111c3376c1486f3e9c7a9d7
cquin <cquin@LAPTOP-PMCSLMQI>
2019-02-18 21:14:22 -0700

    Initial Calculator class

---
+++ CSE360_hw2/src/cse360assign3/Calculator.java
@@ -0,0 +1,103 @@
+/**
+ * NAME: Christian Quintana
+ * Class ID: 527
+ * Student ID: 1210967807
+ * Assignment: Assignment 2
+ * 
+ * Description: The following file contains an unfinished calculator
+ * program. The program contains methods to display total value, add,
+ * subtract, multiply, divide, and show history. These methods are blank
+ * for now and will be added later.
+ */
+
+
+package cse360assign3;
+
+/**
+ * 
+ * @author cquin
+ *
+ * The class Calculator performs basic arithmetic operations such as
+ * add, subtract, multiply, and divide. 
+ * There is also a getHistory method that returns previously entered expressions.
+ * The constructor initializes the total count to zero.
+ * getTotal method returns 0.
+ * Add, subtract, multiply, divide, and getHistory methods are blank and unfinished.
+ *
+ */
+
+public class Calculator {
+
+	private int total;	// class variable
+	
+	/**
+	 * Class constructor instantiates class with total value of 0.
+	 * @param none
+	 * 
+	 */
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * getTotal method returns value of 0.
+	 * 
+	 * 
+	 * @return 0.
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add method is currently blank
+	 * 
+	 * @param value
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract method is currently blank
+	 * 
+	 * @param value
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply method is currently blank
+	 * 
+	 * @param value
+	 */
+	
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide method is currently blank
+	 * 
+	 * @param value
+	 */
+	
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * getHistory method is currently blank
+	 * 
+	 * @param empty string
+	 */
+	
+	public String getHistory () {
+		return "";
+	}
+}
